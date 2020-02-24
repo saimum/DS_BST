eced1b238cfa8a64b9e8e365c32d987c1e8c222c
saimum <saimum720@gmail.com>
2020-02-24 15:56:24 +0600

    first commit

---
+++ DS_BST/BTS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_BST
+{
+    public class BTS
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+        public Node root;
+        public BTS()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- DS_BST/Program.cs
+++ DS_BST/Program.cs
@@ -10,12 +10,16 @@ namespace DS_BST
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            BTS bts = new BTS();
+            bts.Insert(50);
+            bts.Insert(17);
+            bts.Insert(23);
+            bts.Insert(12);
+            bts.Insert(19);
+            
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
